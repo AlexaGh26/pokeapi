f1680cb88fa66f95273698252559c2c7d4852a8c
AlexaGh26 <alexagh26@gmail.com>
2023-10-29 18:48:38 -0500

    infra init

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dmpn6gaBJAboAxPfRUpTjhp7ASDqsu70VKmbdxFoUOU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"a/72q9N3hD4DA7RrWbDhGUWwgBIjAWSpOfuLAymVWDY=\"\n}"
